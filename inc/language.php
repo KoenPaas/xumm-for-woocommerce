cdd1aa35adc3cba40e75423aff4ed85c8a5dcf23
Koen XRPL Labs <koen@paas.nl>
2020-11-04 09:24:41 +0100

    [FIX] locale variable fix

--- inc/language.php
+++ inc/language.php
@@ -1,9 +1,12 @@
 <?php
 $dir = dirname(plugin_dir_path(__FILE__));
 
-$locale = $dir . '/languages/xumm-payment.'.get_locale().'.json';
-if(file_exists($locale)) {
-    $lang = json_decode(file_get_contents($locale));
+$loc = get_locale();
+$loc =  str_replace('_formal', '', $loc);
+
+$language = $dir . '/languages/xumm-payment.'.$loc.'.json';
+if(file_exists($language)) {
+    $lang = json_decode(file_get_contents($language));
 } else {
     $lang = json_decode(file_get_contents($dir . '/languages/xumm-payment.en_EN.json'));
 }
